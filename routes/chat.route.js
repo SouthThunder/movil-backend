99950f5fab4164ea6ef532d848d54a113727f4c1
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-05-16 20:51:23 -0500

    feature: added ws to chat

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { getChatsByUser } from "../controllers/chat.controller.js";
+import { getChatsByUser, webSocketChat } from "../controllers/chat.controller.js";
 import { authorize } from "../controllers/authMiddleware.js";
+
+// Import socket
+import expressWs from "express-ws";
+
 
 // Create a new router
 const router = express.Router();
 
+expressWs(router)
+
 // get all chats by user
 router.get("/chat", authorize, getChatsByUser);
+
+// Chat web socket
+router.ws("/chat/ws", authorize, webSocketChat)
 
 
 export default router
